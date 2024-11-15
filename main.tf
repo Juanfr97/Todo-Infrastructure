acf541620f7d40c3ff3bbe6cff47d3baaa708f5f
Juan Frausto <juanfr97@hotmail.com>
2024-11-15 12:00:02 -0600

    Updated docker compose

--- main.tf
+++ main.tf
@@ -46,11 +46,16 @@ resource "digitalocean_droplet" "juan_server_droplet" {
   provisioner "remote-exec" {
     inline = [
       "mkdir -p /projects",
+      "mkdir -p /volumes/nginx/html",
+      "mkdir -p /volumes/nginx/certs",
+      "mkdir -p /volumes/nginx/vhostd",
       "touch /projects/.env",
       "echo \"MYSQL_DB=${var.MYSQL_DB}\" >> /projects/.env",
       "echo \"MYSQL_USER=${var.MYSQL_USER}\" >> /projects/.env",
       "echo \"MYSQL_HOST=${var.MYSQL_HOST}\" >> /projects/.env",
       "echo \"MYSQL_PASSWORD=${var.MYSQL_PASSWORD}\" >> /projects/.env",
+      "echo \"DOMAIN=${var.DOMAIN}\" >> /projects/.env",
+      "echo \"USER_EMAIL=${var.USER_EMAIL}\" >> /projects/.env",
     ]
     connection {
       type        = "ssh"
@@ -72,26 +77,26 @@ resource "digitalocean_droplet" "juan_server_droplet" {
   }
 }
 
-# resource "time_sleep" "wait_docker_install" {
-#     depends_on = [ digitalocean_droplet.juan_server_droplet ]
-#     create_duration = "130s"
-# }
+resource "time_sleep" "wait_docker_install" {
+    depends_on = [ digitalocean_droplet.juan_server_droplet ]
+    create_duration = "130s"
+}
 
-# resource "null_resource" "init_api" {
-#   depends_on = [ time_sleep.wait_docker_install ]
-#   provisioner "remote-exec" {
-#     inline = [ 
-#       "cd /projects",
-#       "docker-compose up -d" 
-#     ]
-#     connection {
-#       type = "ssh"
-#       user = "root"
-#       private_key = file("./keys/juan_server")
-#       host = digitalocean_droplet.juan_server_droplet.ipv4_address
-#     }
-#   }
-# }
+resource "null_resource" "init_api" {
+  depends_on = [ time_sleep.wait_docker_install ]
+  provisioner "remote-exec" {
+    inline = [ 
+      "cd /projects",
+      "docker-compose up -d" 
+    ]
+    connection {
+      type = "ssh"
+      user = "root"
+      private_key = file("./keys/juan_server")
+      host = digitalocean_droplet.juan_server_droplet.ipv4_address
+    }
+  }
+}
 
 # resource "null_resource" "init_nginx" {
 #   depends_on = [ time_sleep.wait_docker_install ]
@@ -106,18 +111,18 @@ resource "digitalocean_droplet" "juan_server_droplet" {
 #   }
 # }
 # # Copiar carpeta adidas a servidor
-resource "null_resource" "adidas_copy_folder" {
-  provisioner "file" {
-    source = "./adidas"
-    destination = "/adidas"
-  }
-  connection {
-    type = "ssh"
-    user = "root"
-    private_key = file("./keys/juan_server")
-    host = digitalocean_droplet.juan_server_droplet.ipv4_address
-  }
-}
+# resource "null_resource" "adidas_copy_folder" {
+#   provisioner "file" {
+#     source = "./adidas"
+#     destination = "/adidas"
+#   }
+#   connection {
+#     type = "ssh"
+#     user = "root"
+#     private_key = file("./keys/juan_server")
+#     host = digitalocean_droplet.juan_server_droplet.ipv4_address
+#   }
+# }
 # # 1. Hacer cd /
 # # 2. docker cp adidas/. Adidas:/usr/share/nginx/html
 
